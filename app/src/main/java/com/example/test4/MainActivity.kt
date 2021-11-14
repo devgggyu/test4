f3defe6f82148dfb9022cacdf567a5e0949b5b27
studentDG <daegu776@gmail.com>
2021-11-14 17:15:41 +0900

    길게 누룰시 - > 학생 목록 삭제. -> 리스트뷰에 반영(notifyDataSetChanged())

--- app/src/main/java/com/example/test4/MainActivity.kt
+++ app/src/main/java/com/example/test4/MainActivity.kt
@@ -40,9 +40,10 @@ class MainActivity : AppCompatActivity() {
 
          studentListView.setOnItemLongClickListener { parent, view, position, id ->
 
-             val longClickedStudent = mStudentList[position]
 
-             Toast.makeText(this, "${longClickedStudent.name} 길게 클릭됨", Toast.LENGTH_SHORT).show()
+             mStudentList.removeAt(position)
+
+             mAdapter.notifyDataSetChanged()
 
              return@setOnItemLongClickListener true
 
